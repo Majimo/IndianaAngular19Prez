a4c2e60aab8f2c4880bb01b391be00b9bc976667
Pierre Fervel <pierre.fervel@gmail.com>
2025-04-25 16:37:01 +0200

    v0.0.1 : Slides presents

---
+++ README.md
@@ -0,0 +1,1 @@
+# IndianaAngular19Prez
---
+++ main.js
@@ -0,0 +1,13 @@
+import Reveal from './node_modules/reveal.js/dist/reveal.esm.js';
+import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js';
+import RevealHighlight from './node_modules/reveal.js/plugin/highlight/highlight.esm.js';
+
+let deck = new Reveal({
+  plugins: [Markdown, RevealHighlight],
+});
+deck.initialize({
+  width: 1920,
+  height: 974,
+  minScale: 0.5,
+  maxScale: 0.9,
+});
