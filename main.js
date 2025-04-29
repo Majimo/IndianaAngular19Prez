ded307fdf157d6b8cfb8b4f729769dfd9fcab935
Pierre Fervel <pierre.fervel@gmail.com>
2025-04-29 09:08:01 +0200

    v0.0.6 : Angular 13 to 19 more details

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import Reveal from './node_modules/reveal.js/dist/reveal.esm.js';
-import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from './node_modules/reveal.js/plugin/highlight/highlight.esm.js';
 import RevealNotes from './node_modules/reveal.js/plugin/notes/notes.esm.js';
 
 let deck = new Reveal({
-  plugins: [Markdown, RevealHighlight, RevealNotes],
+  plugins: [RevealHighlight, RevealNotes],
 });
 deck.initialize({
   width: 1920,
